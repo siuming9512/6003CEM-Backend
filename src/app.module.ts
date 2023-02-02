f40582756032d4a93cb5e9469ba5bb8ef818e4c2
anthony.on@test.com <anthony.on@test.com>
2023-02-02 21:16:04 +0800

    add feed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
+import { FeedsModule } from './feeds/feeds.module';
 
 @Module({
-  imports: [PetsModule],
+  imports: [PetsModule, FeedsModule],
   controllers: [AppController],
   providers: [AppService],
 })
